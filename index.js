0ed1d6752dd6914767acae6daa70091127ad9c71
Carlos Godoy <carlosgodoy@pursuit.org>
2023-09-11 20:56:20 -0400

    starter code

---
+++ index.js
@@ -0,0 +1,95 @@
+const spiders = require('./data.json')
+console.log(spiders)
+
+
+/**
+ * Calculate the average age of characters in an array.
+ *
+ * @param {Object[]} characters - An array of character objects.
+ * @returns {number} The average age of the characters.
+ */
+function calculateAverageAge(characters) {
+    
+}
+
+
+
+
+/**
+ * Find the most common superpower among an array of characters.
+ *
+ * @param {Object[]} characters - An array of character objects.
+ * @returns {string} The most common superpower.
+ */
+function mostCommonSuperPower(characters) {
+    
+}
+
+
+
+  
+  
+
+/**
+ * Filter characters by age in an array.
+ *
+ * @param {Object[]} characters - An array of character objects.
+ * @param {number} maxAge - The maximum age to filter by.
+ * @returns {Object[]} An array of character objects younger than or equal to the specified age.
+ */
+function filterCharactersByAge(characters, maxAge) {
+    
+}
+  
+
+
+
+
+
+
+/**
+ * List unique superpowers from an array of characters.
+ *
+ * @param {Object[]} characters - An array of character objects.
+ * @returns {string[]} An array of unique superpowers.
+ */
+function listUniqueSuperpowers(characters) {
+    
+}
+
+
+
+/**
+ * Count the total number of 'friend' relations in the 'friends' arrays of all characters.
+ *
+ * @param {Object[]} characters - An array of character objects.
+ * @returns {number} The total count of 'friend' relations in the 'friends' arrays.
+ */
+function countAllFriends(characters) {
+   
+}
+  
+
+
+
+  
+
+
+// Character Statistics: 🔥
+
+// Write a function called characterStatistics that takes an array of characters as input and returns an object containing various statistics about the characters. The object should have the following properties:
+
+// totalCharacters: The total number of characters in the array.
+// mostCommonSuperpower: The most common superpower among the characters.
+// originCounts: An object where the keys are unique origins, and the values are the counts of characters from each origin.
+
+/**
+ * Generate statistics about the characters in the array.
+ *
+ * @param {Object[]} characters - An array of character objects.
+ * @returns {Object} An object containing various statistics about the characters.
+ */
+function characterStatistics(characters) {
+    
+}
+  
